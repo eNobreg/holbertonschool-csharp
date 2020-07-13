4f2d4036b61a8d4d83f6271df54ce54ccc8f9503
eNobreg <ezra.nobrega@outlook.com>
2020-07-13 14:18:43 -0400

    [New] Messing with specifiers

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        Console.WriteLine("Percent: {0:P2}", percent);
+        Console.WriteLine("Currency: {0}", currency.ToString("C", new CultureInfo("en-US")));
+	}
+}
