b638661173f88feeabcc21de0225217204269183
eNobreg <ezra.nobrega@outlook.com>
2020-07-14 13:56:38 -0400

    [New] New task folder

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Number 
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0) {
+            number = number * -1;
+        }
+        int last_digit = number % 10;
+        Console.Write(last_digit);
+        return(last_digit);
+    }
+
+}
