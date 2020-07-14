8114feb9973364ed2a00381eaee9798fbd80d668
eNobreg <ezra.nobrega@outlook.com>
2020-07-14 15:57:15 -0400

    new task file

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            for (counter = 1; counter <= 100; counter++) {
+                if (counter % 3 == 0 && counter % 5 == 0)
+                    Console.Write("FizzBuzz");
+                else if (counter % 5 == 0) {
+                    Console.Write("Buzz");
+                }
+                else if (counter % 3 == 0) {
+                    Console.Write("Fizz");
+                }
+                else
+                {
+                    Console.Write(counter);
+                }
+                if (counter != 100) {
+                    Console.Write(" ");
+                }
+                else {
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
