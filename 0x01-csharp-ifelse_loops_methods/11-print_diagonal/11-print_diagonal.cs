91908107dca2ce404accd318a3ca545cfd17b8c9
eNobreg <ezra.nobrega@outlook.com>
2020-07-14 14:30:02 -0400

    [New] Task to print a diagonal line on the terminal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line 
+{
+    public static void PrintDiagonal(int length)
+    { 
+        string line = "";
+        int counter = 0;
+        if (length > 0) {
+            for (counter = 1; counter <= length; counter++) {
+                line = new String(' ', counter - 1);
+                Console.WriteLine("{0}\\", line);
+            }
+        }
+        Console.Write("\n");
+    }
+}
