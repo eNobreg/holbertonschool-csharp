eef809807288b86550e6a8b863b23f97d408fdb5
eNobreg <ezra.nobrega@outlook.com>
2020-07-14 12:46:57 -0400

    [Update] Changed to using lowercase hex letters

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,7 @@ class Program
     {
         int counter = 0;
         for (counter = 0; counter <= 98; counter++) {
-            Console.Write("{0:D} = 0x{0:X}\n", counter);
+            Console.Write("{0:D} = 0x{0:x}\n", counter);
         }
     }
 }
