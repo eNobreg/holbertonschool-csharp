6c6edde9b0395a9aa191af8ff19a6fb929f6f60e
eNobreg <ezra.nobrega@outlook.com>
2020-07-14 12:38:14 -0400

    [Update] Swapped checks

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -10,11 +10,11 @@ class Program
         if (last_digit > 5) {
             Console.WriteLine("The last digit of {0} is {1} and is greater than 5", number, last_digit);
         }
-        else if (last_digit == 0) {
-            Console.WriteLine("The last digit of {0} is {1} and is zero", number, last_digit);
-        }
-        else {
+        else if (last_digit < 6 && last_digit != 0) {
             Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, last_digit);
+        }
+        else {           
+            Console.WriteLine("The last digit of {0} is {1} and is zero", number, last_digit);
         }
     }
 }
