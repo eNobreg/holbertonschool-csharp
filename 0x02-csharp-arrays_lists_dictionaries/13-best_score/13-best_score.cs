2c0bd3a59afce212f70ac8d10f987efd4027d517
eNobreg <ezra.nobrega@outlook.com>
2020-07-16 17:07:28 -0400

    [New] New file for best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string stored_key = "None";
+        int stored_value = 0;
+        foreach(KeyValuePair<string, int> entry in myList) {
+            if (stored_value < entry.Value)
+                stored_key = entry.Key;
+        }
+        return (stored_key);
+    }
+}
