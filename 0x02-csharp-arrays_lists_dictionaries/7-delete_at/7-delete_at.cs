e6726928f1f64f15afa38030aadf7df8ee0b1d21
eNobreg <ezra.nobrega@outlook.com>
2020-07-16 14:04:30 -0400

    [Update] Fixed error message

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -6,10 +6,10 @@ class List
     public static List<int> DeleteAt(List<int> myList, int index)
     {
         if (index < 0 || index > myList.Count - 1) {
-            Console.WriteLine("Index out of range");
+            Console.WriteLine("Index is out of range");
             return myList;
         }
-        myList.RemoveRange(index, 0);
+        myList.RemoveRange(index, 1);
         return(myList);
     }
 }
