b534b2a424745554b3ee0558f481d0e1dd063875
eNobreg <ezra.nobrega@outlook.com>
2020-07-16 12:45:47 -0400

    [New] Fourth task reversing an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        int counter = 0;
+
+        if (array != null)
+            for (counter = array.Length - 1; counter >= 0; counter--)
+            {
+                Console.Write(array[counter]);
+                if (counter != 0) {
+                    Console.Write(" ");
+                }
+            }
+        Console.WriteLine("");
+    }
+}
