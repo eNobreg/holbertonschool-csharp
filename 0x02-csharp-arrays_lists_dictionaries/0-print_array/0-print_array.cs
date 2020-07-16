fa857061da60eb5fcfadc2665c55e09bd6e99268
eNobreg <ezra.nobrega@outlook.com>
2020-07-16 12:20:54 -0400

    [New] Initial push for the repo

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+class Array
+{
+	public static int[] CreatePrint(int size)
+	{
+        if (size < 0) 
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+		int[] new_array = Enumerable.Range(0, size).ToArray();
+
+        foreach(int number in new_array) {
+			Console.Write(number);
+            if (number != size - 1)
+				Console.Write(" ");
+		}
+		Console.WriteLine("");
+		return (new_array);
+	}
+}
