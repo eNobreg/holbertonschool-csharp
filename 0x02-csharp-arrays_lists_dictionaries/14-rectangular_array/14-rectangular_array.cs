fa9bf62a548e75b7cb4f1fcab2a592a743f1e802
eNobreg <ezra.nobrega@outlook.com>
2020-07-16 17:33:53 -0400

    [New] Print 2d array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5];
+        array[2, 2] = 1;
+        PrintArray(array);
+    }
+    public static void PrintArray(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
