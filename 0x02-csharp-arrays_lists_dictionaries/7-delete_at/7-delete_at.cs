62611926e7978f58105efb875fa11d702954a962
eNobreg <ezra.nobrega@outlook.com>
2020-07-16 14:01:00 -0400

    [New] Just testing something dumb

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List 
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index > myList.Count - 1) {
+            Console.WriteLine("Index out of range");
+            return myList;
+        }
+        myList.RemoveRange(index, 1);
+        return(myList);
+    }
+}
