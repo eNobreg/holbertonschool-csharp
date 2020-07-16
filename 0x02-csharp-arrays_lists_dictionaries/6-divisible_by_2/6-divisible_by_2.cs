b12254e3e15ce522ce1915d82c59c6adec1f952d
eNobreg <ezra.nobrega@outlook.com>
2020-07-16 13:47:30 -0400

    [New] Checking if elements are divisible two

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List 
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        int counter = 0;
+        List<bool> result = new List<bool>();
+
+        for (counter = 0; counter <= myList.Count - 1; counter++) {
+            result.Add((myList[counter] % 2 == 0) ? true : false);
+        }
+        return (result);
+    }
+}
