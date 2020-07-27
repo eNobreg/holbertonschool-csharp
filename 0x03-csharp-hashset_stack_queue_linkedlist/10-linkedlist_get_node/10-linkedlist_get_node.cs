3844b9ad3279799152b2b7d7c30085ea6937676e
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 16:01:19 -0400

    [New] Retrive value at node of a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        List<int> myList = new List<int>(myLList);
+        return (n > myList.Count ? 0 : myList[n]);
+    }
+}
