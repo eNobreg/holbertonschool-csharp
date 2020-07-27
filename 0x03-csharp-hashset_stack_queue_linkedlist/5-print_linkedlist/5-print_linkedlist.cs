50b3cddf4804d1cac69aa3368cee869d341ad5b1
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 14:40:57 -0400

    [New] Task for learning about linked lists, create and print a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        
+        LinkedList<int> newLinked = new LinkedList<int>(Enumerable.Range(0, size).ToArray());
+        foreach (int entry in newLinked)
+            Console.WriteLine(entry);
+        return (newLinked);
+    }
+}
