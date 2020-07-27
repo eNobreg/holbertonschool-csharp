3da8feb7a7922278816b268f955be09162ea7b3b
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 16:41:33 -0400

    [New] Task to sum the value of all elements of a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        // List<int> myList = new List<int>(myLList);
+        // return (n > myList.Count - 1 || n < 0 ? 0 : myList[n]);
+        LinkedListNode<int> current = myLList.First;
+        int total = 0;
+        while (current != null) {
+            total += current.Value;
+            current = current.Next;
+        }
+        return(total);
+    }
+}
