e104e0084a2ed0d838162c59dcb0abcb347ad715
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 15:04:12 -0400

    [New] Creating new node for a linked list and adding it

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        myLList.AddFirst(newNode);
+        return (newNode);
+    }
+}
