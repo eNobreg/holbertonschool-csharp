c0197abf7947eea23ce52a90f0855f05fb6c9f96
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 14:49:29 -0400

    [New] Something real stupid for one line

--- 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -6,8 +6,7 @@ class LList
 {
     public static LinkedList<int> CreatePrint(int size)
     {
-        
-        LinkedList<int> newLinked = new LinkedList<int>(Enumerable.Range(0, size).ToArray());
+        LinkedList<int> newLinked = new LinkedList<int>( size > 0 ? Enumerable.Range(0, size).ToArray() : Enumerable.Range(0,0).ToArray());
         foreach (int entry in newLinked)
             Console.WriteLine(entry);
         return (newLinked);
