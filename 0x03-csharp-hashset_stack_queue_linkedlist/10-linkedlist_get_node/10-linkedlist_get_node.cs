b3cc22a247b8b59fb7fd6e281083814d8bb47486
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 16:32:59 -0400

    [Update] Trying an actual method

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -6,7 +6,19 @@ class LList
 {
     public static int GetNode(LinkedList<int> myLList, int n)
     {
-        List<int> myList = new List<int>(myLList);
-        return (n > myList.Count - 1 || n < 0 ? 0 : myList[n]);
+        // List<int> myList = new List<int>(myLList);
+        // return (n > myList.Count - 1 || n < 0 ? 0 : myList[n]);
+        LinkedListNode<int> current = myLList.First;
+        int index = 0;
+        if (n > myLList.Count - 1 || n < 0)
+            return (0);
+        while (current != null) {
+            if (index == n)
+                return (current.Value);
+            index += 1;
+            current = current.Next;
+            
+        }
+        return(0);
     }
 }
