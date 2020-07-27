1ad1eacc682ba7b91a08014106e3092d355b5077
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 15:42:25 -0400

    [New] Implementing a pop function for linked lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+            return (0);
+
+        int first_node = myLList.First.Value;
+        myLList.RemoveFirst();
+        return (first_node);
+    }
+}
