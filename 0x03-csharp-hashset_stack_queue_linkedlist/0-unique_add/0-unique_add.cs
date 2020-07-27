8479cc5e0674f55c7a1e10fc720a56f8e43fea44
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 13:13:18 -0400

    [New] Initial push for project repository with first task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int total = 0;
+        foreach (int entry in myList.Distinct().ToList()) {
+            total += entry;
+        }
+        return(total);
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,1 @@
+# New types of data structures
