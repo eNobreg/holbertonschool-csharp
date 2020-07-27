e6091fb19291cf8e410a8cd7256704b3e7aabf8e
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 13:52:33 -0400

    [New] Same as last task, just experimenting with functions of a queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count > 0)
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        else
+            Console.WriteLine("Queue is empty");
+        
+        aQueue.Enqueue(newItem);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+
+        if (aQueue.Contains(search)) 
+        {
+            while (aQueue.Count > 0) {
+                string store = aQueue.Peek();
+                aQueue.Dequeue();
+                if (store == search)
+                    break;
+            }
+        }
+        return (aQueue);
+    }
+}
