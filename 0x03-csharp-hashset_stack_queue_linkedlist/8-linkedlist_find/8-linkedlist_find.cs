fea414505fea581d2fc4aab9fe23633510f9892d
eNobreg <ezra.nobrega@outlook.com>
2020-07-27 15:32:26 -0400

    [New] Find the index of an element in a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        List<int> myList = new List<int>(myLList);
+        return (myList.IndexOf(value));
+    }
+}
