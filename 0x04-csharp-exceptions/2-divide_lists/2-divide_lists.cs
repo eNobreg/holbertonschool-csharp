bb8bd543f0a65bdd0c6693771db209e68cce48fc
eNobreg <ezra.nobrega@outlook.com>
2020-07-28 03:30:03 -0400

    [New] New task file for dividing a list elmentwise

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> resultList = new List<int>();
+        int i = 0;
+        int result = 0;
+        for (i = 0; i < listLength; i++) {
+            try {
+                result = list1[i] / list2[i];
+                resultList.Add(result);
+            } catch (DivideByZeroException) {
+                Console.WriteLine("Cannot divide by zero");
+                resultList.Add(0);
+            } catch (ArgumentOutOfRangeException) {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return(resultList);
+
+    }
+}
