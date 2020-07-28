d4b95b4a928dcefc79bde77ff5cde5f670931595
eNobreg <ezra.nobrega@outlook.com>
2020-07-28 03:27:07 -0400

    [New] First task printing a list safely while not worrying about index range

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int printed = 0;
+        for(int i = 0; i < n; i++) {
+            try {
+                printed++;
+                Console.WriteLine("{0}", myList[i]);
+            } catch {
+                printed--;
+            }
+        }
+        return(printed);
+    }
+}
