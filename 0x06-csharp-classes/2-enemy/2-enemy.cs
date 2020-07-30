9dd9f6f0709028031d6e251238b6bea0dde8b430
eNobreg <ezra.nobrega@outlook.com>
2020-07-30 17:10:26 -0400

    [New] Expanding on the zombie class with an additional constructor

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie Enemy class
+    /// 
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Health variable for each enemy
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initalizes a new instance of a zombie class
+        /// </summary>
+        public Zombie() {
+            health = 0;
+        }
+        /// <summary>
+        /// Class contructor for the zombie, getter for value
+        /// </summary>
+        /// <param name="value"> Health value </param>
+        public Zombie(int value) {
+            if (value >= 0) {
+                health = value;
+            }
+            else if (value < 0) {
+                throw new System.Exception("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
