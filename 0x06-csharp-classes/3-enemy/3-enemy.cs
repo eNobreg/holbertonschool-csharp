d47e4d9398a056f4aae4aaac6500642671939dbf
eNobreg <ezra.nobrega@outlook.com>
2020-07-30 17:38:37 -0400

    [New] Set health property to private, added getter for health

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie Enemy class
+    /// 
+    /// </summary>
+    public class Zombie
+    {
+        // Pirvate health variable for the class
+        int health;
+
+        /// <summary>
+        /// Initalizes a new instance of a zombie class
+        /// </summary>
+        public Zombie() {
+            health = 0;
+        }
+        /// <summary>
+        /// Class contructor for the zombie, getter for value
+        /// </summary>
+        /// <param name="value">Health value</param>
+        public Zombie(int value) {
+            if (value >= 0) {
+                health = value;
+            }
+            else if (value < 0) {
+                throw new System.Exception("Health must be greater than or equal to 0");
+            }
+        }
+        /// <summary>
+        /// Getter for the zombier object's health
+        /// </summary>
+        /// <returns>The current zombie object's health</returns>
+        public int GetHealth() {
+            return health;
+        }
+    }
+}
