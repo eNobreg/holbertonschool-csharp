3158810d586999c94badeedf850587143a6729aa
eNobreg <ezra.nobrega@outlook.com>
2020-07-30 17:00:54 -0400

    [New] Expanded the zombie class with a contructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie Enemy class
+    /// 
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Health variable for each enemy
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initalizes a new instance of a zombie class
+        /// </summary>
+        public Zombie() {
+            health = 0;
+        }
+    }
+}
