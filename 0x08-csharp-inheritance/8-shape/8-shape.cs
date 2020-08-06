9952858ed391f00152d1d251e469b064cf3e3a9e
eNobreg <ezra.nobrega@outlook.com>
2020-08-06 16:35:32 -0400

    [New] Push class update, added square class

---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape {
+    private int width;
+    private int height;
+    public int Width 
+    {
+        get
+        {
+            return width;
+        } set
+        {
+            if (value < 0)
+                throw new System.ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return height;
+        } set
+        {
+            if (value < 0)
+                throw new System.ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+    public new int Area()
+    {
+        return(height * width);
+    }
+
+    public override string ToString()
+    {
+        return($"[Rectangle] {width} / {height}");
+    }
+}
+
+class Square : Rectangle {
+    private int size;
+    public int Size
+    {
+        get 
+        {
+            return size;
+        } set
+        {
+            if (value < 0)
+                throw new System.ArgumentException("Size must be greater than or equal to 0");
+            
+            size = value;
+            Height = value;
+            Width = value;
+        }
+    }
+}
