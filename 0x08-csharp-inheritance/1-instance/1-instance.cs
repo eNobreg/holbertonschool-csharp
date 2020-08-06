f3f1e54c532649f85bd3c65cc5947d2e4d5e95f2
eNobreg <ezra.nobrega@outlook.com>
2020-08-06 14:56:21 -0400

    [New] Push method to check if a given object is the instance of another

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+   {
+        if (obj is Array)
+            return (true);
+        else
+            return (false);
+    }
+}
