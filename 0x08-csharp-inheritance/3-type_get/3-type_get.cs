f1b07f58627ec4cf2fca77ea31af22b33a0e12bc
eNobreg <ezra.nobrega@outlook.com>
2020-08-06 15:24:28 -0400

    [New] Push method to list all methods and properties of a class

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Type t = myObj.GetType();
+
+        IEnumerable<PropertyInfo> propertyList = t.GetProperties();
+        IEnumerable<MethodInfo> methodList = t.GetMethods();
+
+        Console.WriteLine(t.Name + " Properties:");
+
+        foreach(var entry in propertyList)
+        {
+            Console.WriteLine(entry.Name);
+        }
+
+        Console.WriteLine(t.Name + " Methods:");
+
+        foreach(var entry in methodList)
+        {
+            Console.WriteLine(entry.Name);
+        }
+    }
+}
