d2ea01023c50647caf613ceee4f2e5809d4a878c
eNobreg <ezra.nobrega@outlook.com>
2020-08-06 15:43:01 -0400

    [New] Added basic square class

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+}
