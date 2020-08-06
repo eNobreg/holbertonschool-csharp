a29cc358c03859c9e788c3be15c8d01de3259c0d
eNobreg <ezra.nobrega@outlook.com>
2020-08-06 16:23:08 -0400

    [New] Implementing two override methods for the class

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape {
+    private int width;
+    private int height;
+    public int Width 
+    {
+        get
+        {
+            return width;
+        } set
+        {
+            if (value < 0)
+                throw new System.ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return height;
+        } set
+        {
+            if (value < 0)
+                throw new System.ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+    public new int Area()
+    {
+        return(height * width);
+    }
+
+    public override string ToString()
+    {
+        return($"[Rectangle] {width} / {height}");
+    }
+}
