41ed4d65070135f289b6fbf082378cee590e2d1e
eNobreg <ezra.nobrega@outlook.com>
2020-08-06 14:56:46 -0400

    [New] Push method that checks if given object is just a subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+   {
+        if (derivedType.IsSubclassOf(baseType))
+            return (true);
+        else
+            return (false);
+    }
+}
