649366888ecba2e1525680720f41aecc148b178e
eNobreg <ezra.nobrega@outlook.com>
2020-08-17 00:57:16 -0400

    [New] Image Processor first task

---
+++ image_processor/image_processor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+class ImageProcessor
+{
+    public static void Inverse(string[] filenames)
+    {
+        foreach (string file in filenames)
+        {
+            Thread t = new Thread(ImageProcessor.invert_image);
+            t.Start(file);
+        }
+    }
+
+    public static void invert_image(object data)
+    {        
+        string file = data.ToString();
+        int img_width;
+        int img_height;
+        string filename = "";
+        string extension = "";
+
+        Bitmap bmp = new Bitmap(file);
+        img_width = bmp.Width;
+        img_height = bmp.Height;
+        filename = file.Substring(7, file.Length - 11);
+        extension = file.Substring(file.Length - 4, 4);
+        Console.WriteLine(extension);
+        Console.WriteLine(filename);
+
+        for (int y = 0; y < img_height; y++)
+        {
+            for (int x = 0; x < img_width; x++)
+            {
+                Color p = bmp.GetPixel(x, y);
+
+                int r = 255 - p.R;
+                int g = 255 - p.G;
+                int b = 255 - p.B;
+                int a = p.A;
+
+                bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+            }
+        }
+        bmp.Save(filename + "_inverse" + extension);
+        Console.WriteLine($"{filename} done");
+    }
+}
