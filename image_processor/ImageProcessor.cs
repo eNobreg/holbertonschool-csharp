74fb3d5cc079d61713be351deff38192e62b0d6e
eNobreg <ezra.nobrega@outlook.com>
2020-08-18 17:12:07 -0400

    [Update] Added BW conversion

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -104,4 +104,55 @@ class ImageProcessor
         bmp.Save(filename + "_grayscale" + extension);
     }
 
+    public static void BlackWhite(string[] filenames, double threshold)
+    {
+        Parallel.ForEach (filenames, file =>
+        {
+			blackwhite_image(file, threshold);
+        });
+    }
+
+    public static void blackwhite_image(object data, double threshold)
+    {        
+        string file = data.ToString();
+		float result;
+        string filename = "";
+        string extension = "";
+
+        Bitmap bmp = new Bitmap(file);
+        extension = Path.GetExtension(file);
+        filename = Path.GetFileNameWithoutExtension(file);
+        // Lock the bitmap's bits.  
+        Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+        BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+        IntPtr ptr = bmpData.Scan0;
+
+        int bytes  = Math.Abs(bmpData.Stride) * bmp.Height;
+        byte[] rgbValues = new byte[bytes];
+
+        System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+        // Set every third value to 255. A 24bpp bitmap will look red.  
+        for (int i = 0; i < rgbValues.Length - 2; i += 3)
+		{
+			result = rgbValues[i] + rgbValues[i + 1] + rgbValues[i + 2];
+			if (result >= threshold)
+			{
+				rgbValues[i] = 255;
+				rgbValues[i + 1] = 255;
+				rgbValues[i + 2] = 255;
+			}
+			else
+			{
+				rgbValues[i] = 0;
+				rgbValues[i + 1] = 0;
+				rgbValues[i + 2] = 0;
+			}
+		}
+        System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+        bmp.UnlockBits(bmpData);
+        bmp.Save(filename + "_bw" + extension);
+    }
+
 }
