061e4f995f3cba19f9ee8b33356cf2e7994da800
eNobreg <ezra.nobrega@outlook.com>
2020-08-25 15:29:21 -0400

    [New] Function to multiiply 2D and 3D vectors by a scalar

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class VectorMath
+{
+
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+
+        if (vector.Length <= 3 && vector.Length >= 2)
+        {
+            for (int i = 0; i < vector.Length; i++)
+                vector[i] *= scalar;
+            return (vector);
+        }
+        return (new double[1]{-1});
+    }
+}
