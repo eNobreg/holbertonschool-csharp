9c102f5c4e58245d184bab6bd6ca1e9fca22e584
eNobreg <ezra.nobrega@outlook.com>
2020-08-25 13:49:38 -0400

    Testing the script

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class VectorMath 
+{
+    public static double Magnitude(double[] vector)
+    {
+        double total = 0;
+        if (vector.Length <= 3 && vector.Length > 1)
+        {
+            foreach (double entry in vector)
+                total += Math.Pow(entry, 2);
+        }
+        return(Math.Round(Math.Sqrt(total), 2));
+    }
+}
