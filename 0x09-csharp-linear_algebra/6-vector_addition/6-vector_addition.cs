dfc2f51408ec4916ad1eebf561c66e1b95b96546
eNobreg <ezra.nobrega@outlook.com>
2020-08-25 14:44:58 -0400

    [Update] Script to add two vectors together

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        
+        int vectorLength = vector1.Length;
+        double[] final = new double[vectorLength];
+
+        if (vector1.Length == vector2.Length && vectorLength <= 3 && vectorLength >= 2)
+        {
+            for (int i = 0; i < vectorLength; i++)
+                final[i] = vector1[i] + vector2[i];
+            return (final);
+        }
+        return (new double[1]{-1});
+    }
+}
