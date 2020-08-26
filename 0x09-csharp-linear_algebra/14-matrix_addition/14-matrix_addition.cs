fa9ae4b61ebe10d028c99b754b721e71203c39e4
eNobreg <ezra.nobrega@outlook.com>
2020-08-26 12:00:08 -0400

    [New] New method to preform matrix addition

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(0) != matrix2.GetLength(0))
+            return (new double[,]{{-1}});
+
+        if (matrix1.Length == matrix2.Length && matrix1.GetLength(0) >= 2 && matrix1.GetLength(0) <= 3)
+        {
+            if (matrix1.GetLength(1) == matrix2.GetLength(1))
+            {
+                for (int i = 0; i < matrix1.GetLength(0); i++)
+                    for (int j = 0; j < matrix1.GetLength(1); j++)
+                        matrix1[i, j] += matrix2[i, j];
+                return (matrix1);
+            }
+        }
+        return (new double[,]{{-1}});
+    }
+}
