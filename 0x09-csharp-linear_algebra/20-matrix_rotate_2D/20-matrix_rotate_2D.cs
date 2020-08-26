320f1e737d6f4f73a03b923b17d83f169fc45422
eNobreg <ezra.nobrega@outlook.com>
2020-08-26 14:08:40 -0400

    [New] Method to rotate a matrix using a rotation matrix

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double [,] rotationMatrix = new double[,] { {Math.Cos(angle), Math.Sin(angle)}, {Math.Sin(angle) * -1, Math.Cos(angle)}};
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return (new double[,]{{-1}});
+
+        double[,] results = new double[2, 2];
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                results[i, j] = 0;
+                for (int k = 0; k < 2; k++)
+                    results[i, j] += Math.Round(matrix[i, k] * rotationMatrix[k, j], 2);
+            }
+        }
+        return (results);
+    }
+}
