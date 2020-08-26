a6365b63489ab2466e5bf626026218be73c1292b
eNobreg <ezra.nobrega@outlook.com>
2020-08-26 18:27:12 -0400

    [Update] More error checking

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -11,10 +11,13 @@ class MatrixMath
     {
         double det = 0;
 
-        if (matrix.GetLength(0) != matrix.GetLength(1))
+        if (matrix.GetLength(1) > 3 || matrix.GetLength(1) < 2)
             return (-1);
 
         if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+            return (-1);
+
+        if (matrix.GetLength(0) != matrix.GetLength(1))
             return (-1);
 
         if (matrix.GetLength(0) == 3)
