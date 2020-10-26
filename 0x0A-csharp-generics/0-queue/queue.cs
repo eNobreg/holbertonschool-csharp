4ae69eb62f697c9d38d57cb19f84000647bdf0e6
eNobreg <ezra.nobrega@outlook.com>
2020-10-26 15:15:16 -0400

    [New] Initial push

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Queue<T>
+{
+	/// <summary>
+	/// Checks the type of the calling queue/node
+	/// </summary>
+	/// <returns>Returns the type of the queue</returns>
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
+}
