de656828f6a9d6ec75e60434c64b310d88e7890a
eNobreg <ezra.nobrega@outlook.com>
2020-10-28 16:23:00 -0400

    [New] Initial push

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/// <summary>
+/// Player baseclass
+/// </summary>
+public class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+    /// <summary>
+    /// Player constructer
+    /// </summary>
+    /// <param name="name">Name of character</param>
+    /// <param name="maxHp">MaxHP of charater</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    /// <summary>
+    /// prints out health values of characters
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
